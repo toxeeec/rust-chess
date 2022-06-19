ffd71478b3c492ef17af8403f1ac6ffb28a72681
toxeeec <bartosz.kapciak@gmail.com>
2022-06-19 02:10:59 +0200

    feat: change Debug trait to Display, add Debug trait

--- src/chess/bitboard.rs
+++ src/chess/bitboard.rs
@@ -1,9 +1,17 @@
 use std::fmt;
+
+#[derive(PartialEq, Eq)]
 pub struct Bitboard(pub u64);
 
 const BITBOARD_STRING_LENGTH: usize = 16 * 8 - 1;
 
 impl fmt::Debug for Bitboard {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:064b}", self.0)
+    }
+}
+
+impl fmt::Display for Bitboard {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let b: Vec<char> = format!("{:064b}", self.0).chars().rev().collect();
         let mut formatted = String::with_capacity(BITBOARD_STRING_LENGTH);
